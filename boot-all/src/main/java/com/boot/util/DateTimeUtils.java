7d29fbb8f05094dfc5779e157d1e94b6b5bb89e3
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-09-12 17:37:19 +0700

    #

--- boot-all/src/main/java/com/boot/util/DateTimeUtils.java
+++ boot-all/src/main/java/com/boot/util/DateTimeUtils.java
@@ -14,6 +14,7 @@ public class DateTimeUtils {
 	public static final long ONE_MINUTES			=	TimeUnit.MINUTES.toMillis(1);
 	public static final long ONE_HOURS				=	60 * ONE_MINUTES;
 	public static final long ONE_DAYS				=	24 * ONE_HOURS;
+	public static final long ONE_MONTHS				=	30 * ONE_DAYS;
 
 	public static final String HH_MM_SS				=	"HH:mm:ss";
 	public static final String YYYY_MM_DD			=	"yyyy-MM-dd";
