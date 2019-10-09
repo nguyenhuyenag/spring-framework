7496b1fe2a43c5c2295dc99d2c0b5398a411d2d0
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-10-09 16:04:52 +0700

    #

---
+++ hibernate/src/main/java/core/Application.java
@@ -0,0 +1,26 @@
+package core;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	@Override
+	public void run(String... params) throws Exception {
+
+	}
+
+}
