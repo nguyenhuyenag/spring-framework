3020cd6f1740df0f3aa33ad5d915791d7ad12870
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2019-10-22 23:43:58 +0700

    #

--- hibernate/src/main/java/core/entity/generator/GeneratorId.java
+++ hibernate/src/main/java/core/entity/generator/GeneratorId.java
@@ -3,24 +3,23 @@ package core.entity.generator;
 import java.io.Serializable;
 import java.util.stream.Stream;
 
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 import org.hibernate.query.spi.QueryImplementor;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
 
 public class GeneratorId implements IdentifierGenerator {
 
-	private String prefix = "EMP";
+	private final String PREFIX = "EMP";
 
 	@Override
 	public Serializable generate(SharedSessionContractImplementor session, Object object) {
-		// Select all id
 		QueryImplementor<String> query = session.createQuery("SELECT e.id FROM Employee e", String.class);
 		try (Stream<String> stream = query.stream()) {
-			Long max = stream.map(t -> t.replace(prefix, "")) // EMP0001 => 0001
-					.mapToLong(Long::parseLong) // String to Long
-					.max() 						// Tìm số lớn nhất
-					.orElse(0L); 				// Nếu không có thì set là 0
-			return prefix + String.format("%04d", max + 1); // EMP0002
+			Long max = stream.map(t -> t.replace(PREFIX, "")) // EMP0001 => 0001
+							 .mapToLong(Long::parseLong) // String to Long
+							 .max()						 // Tìm số lớn nhất
+							 .orElse(0L); 				 // Nếu không có thì set là 0
+			return PREFIX + String.format("%04d", max + 1); // EMP0002
 		}
 	}
 }
