d0027f07d28f55d28124172da3b7480621080d6e
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-11-13 08:23:50 +0700

    #

--- spring-core/src/main/java/com/di/HelloWorld.java
+++ spring-core/src/main/java/com/di/HelloWorld.java
@@ -1,24 +1,17 @@
 package com.di;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HelloWorld {
 
 	private String message;
-
-	public HelloWorld() {
-		
-	}
-
-	public HelloWorld(String message) {
-		this.message = message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getMessage() {
-		return message;
-	}
 
 	public void print() {
 		System.out.println("Print: " + this.message);
--- spring-core/src/main/java/com/di/Main.java
+++ spring-core/src/main/java/com/di/Main.java
@@ -3,15 +3,27 @@ package com.di;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/*-
+ * Injection by Constructor (CI) vs Injection by Setter (SI)
+ * 
+ * - Inject từng phần: Chỉ có thể inject từng thuộc tính bằng SI.
+ * 
+ * - Overriding: SI ghi đè lại CI, nếu dùng cả 2, IoC sẽ sử dụng SI.
+ * 
+ * - Chuyển đổi: Dễ dàng thay đổi giá trị bằng SI mà ko new instance
+ * 
+ * - Nếu dùng SI thì class phải có default constructor, dùng CI thì phải có hàm
+ * khởi tạo với các tham số tương ứng với injection.
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		// FileSystemXmlApplicationContext("classpath:di/context.xml");
 		try (AbstractApplicationContext context = new ClassPathXmlApplicationContext("di/context.xml");) {
-			HelloWorld helloWorld1 = (HelloWorld) context.getBean("helloWorld1");
-			helloWorld1.print();
-			HelloWorld helloWorld2 = (HelloWorld) context.getBean("helloWorld2");
-			helloWorld2.print();
+			HelloWorld h1 = (HelloWorld) context.getBean("helloWorld1");
+			h1.print();
+			HelloWorld h2 = (HelloWorld) context.getBean("helloWorld2");
+			h2.print();
 		}
 	}
 
