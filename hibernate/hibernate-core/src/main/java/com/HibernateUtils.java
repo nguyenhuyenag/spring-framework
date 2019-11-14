b3abd52f545c0b0508a5adcf5f481c6936ce7ece
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2019-11-14 09:23:43 +0700

    #

--- hibernate/hibernate-core/src/main/java/com/HibernateUtils.java
+++ hibernate/hibernate-core/src/main/java/com/HibernateUtils.java
@@ -4,32 +4,36 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+/**
+ * SessionFactory là đối tượng nặng (heavy weight object) và được sử dụng thường
+ * xuyên nên cần tạo một class singleton HibernateUtils
+ */
 public class HibernateUtils {
-
-	private HibernateUtils() {
-		super();
-	}
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
+	private HibernateUtils() {
+
+	}
+
 	private static SessionFactory buildSessionFactory() {
+		return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	}
+
+	@SuppressWarnings("unused")
+	private static SessionFactory buildSessionFactory2() {
+		Configuration config = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(com.User.class);
+		ServiceRegistry service = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+		SessionFactory sessionFactory = config.buildSessionFactory(service);
+		return sessionFactory;
+	}
+
+	@SuppressWarnings("unused")
+	private static SessionFactory buildSessionFactory3() {
 		try {
-			// cach 1
-			// return new
-			// Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-
-			// cach 2
-			// Configuration config = new
-			// Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(com.User.class);
-			// ServiceRegistry service = new
-			// StandardServiceRegistryBuilder().applySettings(config.getProperties())
-			// .build();
-			// SessionFactory sessionFactory = config.buildSessionFactory(service);
-			// return sessionFactory;
-
-			// cach 3
 			ServiceRegistry service = new StandardServiceRegistryBuilder().configure().build();
 			Metadata metadata = new MetadataSources(service).getMetadataBuilder().build();
 			return metadata.getSessionFactoryBuilder().build();
