496b21833721f73a3697a32ee69e870962807fc1
nguyenhuyenag <43562912+nguyenhuyenag@users.noreply.github.com>
2020-10-09 13:47:06 +0700

    #

--- spring-boot/spring-boot-final/src/main/java/com/entity/User.java
+++ spring-boot/spring-boot-final/src/main/java/com/entity/User.java
@@ -1,6 +1,7 @@
 package com.entity;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -70,27 +71,27 @@ public class User implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return null;
+		return Collections.emptyList();
 	}
 
 	@Override
-	public boolean isAccountNonExpired() {
-		return false;
-	}
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return false;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return false;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return false;
-	}
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
