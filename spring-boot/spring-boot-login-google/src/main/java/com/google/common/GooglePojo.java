e42fc59de3773050ed3a4c7dcc218f6e0f0d9a66
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-03-28 19:59:57 +0700

    #

---
+++ spring-boot/spring-boot-login-google/src/main/java/com/google/common/GooglePojo.java
@@ -0,0 +1,27 @@
+package com.google.common;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GooglePojo {
+
+	private String id;
+	private String email;
+	private boolean verified_email;
+	private String name;
+	private String given_name;
+	private String family_name;
+	private String link;
+	private String picture;
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+	}
+
+}
