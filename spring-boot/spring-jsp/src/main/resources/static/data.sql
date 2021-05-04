689626207f63fe13e5af3137e7f1c498fdecd1c6
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-05-04 15:34:54 +0700

    #

--- spring-boot/spring-jsp/src/main/resources/static/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO 
-  tbl_user (username, password)
-VALUES
-	('stackjava', 'stackjava'),
-	('admin', 'admin1234'),
-	('root', 'root');
--- spring-boot/spring-jsp/src/main/resources/static/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS tbl_user;
- 
-CREATE TABLE tbl_user (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  username VARCHAR(250) NOT NULL,
-  password VARCHAR(250) NOT NULL
-);
