ddd1c65ddab9951f0078de8b827453e66a7cb153
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-06-20 21:28:24 +0700

    update

--- spring-boot/spring-boot-basic/src/main/java/com/boot/ResourceFile.java
+++ spring-boot/spring-boot-basic/src/main/java/com/boot/ResourceFile.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-// @Component
 public class ResourceFile {
 
 	public static File byClassPathResource() {
@@ -19,32 +18,5 @@ public class ResourceFile {
 		}
 		return null;
 	}
-
-//	@Autowired
-//	private ResourceLoader tFoo;
-//
-//	private static ResourceLoader foo;
-//
-//	@PostConstruct
-//	public void init() {
-//		ResourceFile.foo = tFoo;
-//	}
-//
-//	public static File byResourceLoader() {
-//		try {
-//			Resource resource = foo.getResource("data/resource-data.txt");
-//			// return resource.getInputStream();
-//			return resource.getFile();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-//
-////		Resource resource = foo.getResource("classpath:android.png");
-////
-////	    InputStream input = resource.getInputStream();
-////
-////	    File file = resource.getFile();
-//	}
 
 }
