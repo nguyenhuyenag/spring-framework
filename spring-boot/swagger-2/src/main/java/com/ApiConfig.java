057b178ab56f5041d1847c6ec70bc8e0193cb9e8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2021-12-20 09:22:52 +0700

    #

---
+++ spring-boot/swagger-2/src/main/java/com/ApiConfig.java
@@ -0,0 +1,7 @@
+package com;
+//package com.deft.swagger;
+//
+//public class ApiConfig {
+//
+//    public static final String API = "/api/v1";
+//}
--- spring-boot/swagger-2/src/main/java/com/WebSecurityConfig.java
+++ spring-boot/swagger-2/src/main/java/com/WebSecurityConfig.java
@@ -1,5 +1,4 @@
-package com.config;
-
+package com;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -26,13 +25,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService) // cài đặt dịch vụ để tìm kiếm User trong Database
 			.passwordEncoder(passwordEncoder()); // cài đặt PasswordEncoder
 	}
+	
+	private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
-			.antMatchers("/", "/book/*", "/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui/index.html", "/webjars/**").permitAll()
+			.antMatchers(AUTH_WHITELIST).permitAll()
 			.anyRequest().authenticated();
+		
+//		http.authorizeRequests().
+//        antMatchers(AUTH_WHITELIST).permitAll()  // whitelist Swagger UI resources
+//        .anyRequest().authenticated();
 	}
 
 }
