a3cf8e4881818642522797852841cbf12862c83d
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-01-07 17:15:36 +0700

    #

--- spring-boot/spring-boot-security-jwt/src/main/java/com/config/JacksonConfiguration.java
+++ spring-boot/spring-boot-security-jwt/src/main/java/com/config/JacksonConfiguration.java
@@ -1,27 +1,26 @@
-package com.config;
-
-import java.util.TimeZone;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-@Configuration
-public class JacksonConfiguration {
-
-//    @Autowired
-//    public JacksonConfiguration(ObjectMapper objectMapper){
-//        objectMapper.setTimeZone(TimeZone.getDefault());
-//    }
-	
-	@Bean
-	ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-	    return builder.createXmlMapper(false)
-	            // Set timezone for JSON serialization as system timezone
-	            .timeZone(TimeZone.getDefault())
-	            .build();
-	}
-    
-}
+//package com.config;
+//
+//import java.util.TimeZone;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//@Configuration
+//public class JacksonConfiguration {
+//
+////    @Autowired
+////    public JacksonConfiguration(ObjectMapper objectMapper){
+////        objectMapper.setTimeZone(TimeZone.getDefault());
+////    }
+//
+//	@Bean
+//	ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+//		return builder.createXmlMapper(false)
+//				// Set timezone for JSON serialization as system timezone
+//				.timeZone(TimeZone.getDefault()).build();
+//	}
+//
+//}
