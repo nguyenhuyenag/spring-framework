c519e6e6d9eeb804b966ea23d395eef54a01fc72
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-09-27 09:38:06 +0700

    #

---
+++ microservice/service/src/main/java/com/test/service/controllers/ServiceController.java
@@ -0,0 +1,18 @@
+package com.test.service.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceController {
+
+	@Value("${service.instance.name}")
+	private String name;
+
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Hello from " + name;
+	}
+
+}
