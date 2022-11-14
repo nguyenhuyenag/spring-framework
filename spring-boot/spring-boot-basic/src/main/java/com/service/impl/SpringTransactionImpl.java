1b52ad1bc7c026c7411491531e0347c3f2748d89
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-11-14 15:36:13 +0700

    #

--- spring-boot/spring-boot-basic/src/main/java/com/service/impl/SpringTransactionImpl.java
+++ spring-boot/spring-boot-basic/src/main/java/com/service/impl/SpringTransactionImpl.java
@@ -1,9 +1,10 @@
 package com.service.impl;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
@@ -11,6 +12,46 @@ import com.entity.User;
 import com.repository.UserRepository;
 import com.service.SpringTransaction;
 
+/**
+ * > Propagation
+ * 
+ * - REQUIRED: Nếu có một transaction đang hoạt động thì nó sẽ sử dụng chung,
+ * nếu không method được gọi sẽ tạo một transaction mới. Đây là giá trị mặc định
+ * của Propagation.
+ * 
+ * - SUPPORTS: Sử dụng lại transaction hiện đang hoạt động. Nếu không thì method
+ * được gọi sẽ thực thi mà không được đặt trong một transactional context nào
+ * 
+ * - MANDATORY: Yêu cầu phải có một transaction đang hoạt động trước khi gọi,
+ * nếu không method được gọi sẽ ném ra một exception.
+ * 
+ * - NEVER: Ném một exception nếu method được gọi trong một transaction hoạt
+ * động.
+ * 
+ * - NOT_SUPPORTED: Dừng transaction hiện tại và thực thi method mà không thuộc
+ * một transaction nào
+ * 
+ * - REQUIRES_NEW: Luôn bắt đầu một transaction mới cho method được gọi. Nếu
+ * method được gọi với một transaction đang hoạt động, transaction đó sẽ bị tạm
+ * ngưng, một transaction mới sẽ được tạo và sử dụng cho method này. Transaction
+ * mới vừa được tạo sẽ thực thi độc lập với transaction bên ngoài, khi
+ * transaction này kết thúc dữ liệu sẽ được đồng bộ xuống database. Sau đó
+ * transaction bên ngoài sẽ được kích hoạt và hoạt động trở lại.
+ * 
+ * - NESTED: Method được gọi sẽ tạo một transaction mới nếu không có transaction
+ * nào đang hoạt động. Nếu method được gọi với một transaction đang hoạt động
+ * Spring sẽ tạo một savepoint và rollback tại đây nếu có Exception xảy ra.
+ * 
+ * > Read-Only
+ * 
+ * > Handling Exceptions
+ * 
+ * - Spring proxy sẽ tự động rollback transaction nếu có một RuntimeException
+ * xảy ra. Bạn có thể tùy biến bằng cách sử dụng thuộc tính rollbackFor và
+ * noRollbackFor của @Transactional annotation
+ * 
+ *
+ */
 @Service
 public class SpringTransactionImpl implements SpringTransaction {
 
@@ -18,7 +59,7 @@ public class SpringTransactionImpl implements SpringTransaction {
 	UserRepository repository;
 
 	@Override
-	@Transactional(propagation = Propagation.SUPPORTS)
+	@Transactional()
 	public void rollBackWithStatus() {
 		System.out.println(TransactionAspectSupport.currentTransactionStatus());
 		System.out.println(TransactionAspectSupport.currentTransactionStatus().isNewTransaction());
@@ -57,6 +98,15 @@ public class SpringTransactionImpl implements SpringTransaction {
 
 	@Override
 	public void hibernateTransaction() {
+
+	}
+
+	/**
+	 * rollback transaction cho tất cả các sub-class của Exception ngoại trừ
+	 * EntityNotFoundException
+	 */
+	@Transactional(rollbackFor = Exception.class, noRollbackFor = EntityNotFoundException.class)
+	public void updateAuthorWithRollbackCustom(Long id, String name) {
 
 	}
 
