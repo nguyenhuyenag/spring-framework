671b20971d1b2ba3b6b363497970638a23535ff4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-11-28 16:41:04 +0700

    #

--- spring-boot/spring-boot-app/src/main/java/com/Application.java
+++ spring-boot/spring-boot-app/src/main/java/com/Application.java
@@ -3,32 +3,25 @@ package com;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
 
-	// JAR
+	/* WAR */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Autowired
-//	Service service;
-//	
-//	@Autowired
-//	FileStoreService fileStoreService;
-//	
-//	@Autowired
-//	FileStoreRepository fileStoreRepository;
-
 	@Override
 	public void run(String... args) throws Exception {
-		// System.out.println(RandomStringUtils.randomAlphanumeric(20).toUpperCase());
-		// Path path = Paths.get("C:\\Users\\huyennv\\Desktop\\ordinal-numbers-english.png");
-		// String s = Base64Utils.encodeToString(path);
-		// FileStore file = fileStoreService.findById("B5LUDZCNLSJ0VLYDAES0");
-		// file.setFileContent(s.trim());
-		// fileStoreRepository.save(file);
+
 	}
 
 }
