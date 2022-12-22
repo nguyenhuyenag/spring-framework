707528d4964f7989fc4e66dba7d793c01263d090
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2022-12-22 10:54:52 +0700

    #

---
+++ spring-boot/validation-rest/src/main/java/com/validationrest/UserController.java
@@ -0,0 +1,37 @@
+package com.validationrest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+	/**
+	 *	POST -> { "age": 10 }
+	 */
+	@PostMapping("/users")
+	ResponseEntity<String> addUser(@Valid @RequestBody UserDTO userDTO) {
+		return ResponseEntity.ok("User is valid");
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		System.out.println(errors);
+		return errors;
+	}
+
+}
