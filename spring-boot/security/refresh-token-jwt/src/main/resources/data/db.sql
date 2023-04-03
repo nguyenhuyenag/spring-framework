701285c465dc8fcd0b2b5e9c17f18755bd17fd7f
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-04-03 08:21:16 +0700

    #

--- spring-boot/security/refresh-token-jwt/src/main/resources/data/db.sql
+++ spring-boot/security/refresh-token-jwt/src/main/resources/data/db.sql
@@ -1,16 +1,9 @@
-CREATE TABLE `refresh_token` (
+CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
-  `token` varchar(255) DEFAULT NULL,
-  `expiry_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token_UNIQUE` (`token`)
-);
-
-CREATE TABLE `user_roles` (
-  `user_id` int NOT NULL,
-  `role_id` int NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`)
+  `password` varchar(255) DEFAULT NULL,
+  `disable` TINYINT(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `role` (
@@ -20,18 +13,25 @@ CREATE TABLE `role` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `disable` TINYINT(1) DEFAULT '0',
-  PRIMARY KEY (`id`)
+CREATE TABLE `user_roles` (
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
 );
 
-INSERT INTO `user` VALUES 
-	(1,'huyennv','$2a$10$13HWiq1Z0S3vRSd836s3H.0GmceNFjo8HpIOTbZDD4/8KhYAU.yAq'),
-	(2,'dev1','$2a$10$13HWiq1Z0S3vRSd836s3H.0GmceNFjo8HpIOTbZDD4/8KhYAU.yAq'),
-	(3,'user','$2a$10$13HWiq1Z0S3vRSd836s3H.0GmceNFjo8HpIOTbZDD4/8KhYAU.yAq');
+CREATE TABLE `refresh_token` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `expiry_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token_UNIQUE` (`token`)
+);
+
+INSERT INTO `user` (`username`, `password`) VALUES 
+	('huyennv','$2a$10$13HWiq1Z0S3vRSd836s3H.0GmceNFjo8HpIOTbZDD4/8KhYAU.yAq'),
+	('dev1','$2a$10$13HWiq1Z0S3vRSd836s3H.0GmceNFjo8HpIOTbZDD4/8KhYAU.yAq'),
+	('user','$2a$10$13HWiq1Z0S3vRSd836s3H.0GmceNFjo8HpIOTbZDD4/8KhYAU.yAq');
 
 INSERT INTO `role` VALUES 
 	(1,'ADMIN','Admin role'),
