53caa19e616459896082c38b2bae47db4e4d386b
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-04-04 16:39:20 +0700

    #

--- spring-boot/spring-pagination/src/main/java/com/controller/PageApiController.java
+++ spring-boot/spring-pagination/src/main/java/com/controller/PageApiController.java
@@ -18,6 +18,17 @@ public class PageApiController {
 	@Autowired
 	private ProductService productService;
 
+	@GetMapping("/page-info")
+	public ResponseEntity<?> info(HttpServletRequest request) {
+		int page = ServletRequestUtils.getIntParameter(request, "page", 1);
+		int size = ServletRequestUtils.getIntParameter(request, "size", 1);
+		boolean showContent = ServletRequestUtils.getBooleanParameter(request, "showContent", false);
+		if (showContent) {
+			return ResponseEntity.ok(productService.getContent(page, size));
+		}
+		return ResponseEntity.ok(productService.info(request, page, size));
+	}
+	
 //	@GetMapping("test")
 //	public String index(HttpServletRequest request, ModelMap model) {
 //		int page = ServletRequestUtils.getIntParameter(request, "p", 0);
@@ -37,17 +48,5 @@ public class PageApiController {
 //		List<Product> pages = productService.get(page, size, sortby);
 //		return ResponseEntity.ok(pages);
 //	}
-
-	@GetMapping("/page-info")
-	public ResponseEntity<?> info(HttpServletRequest request) {
-		int page = ServletRequestUtils.getIntParameter(request, "page", 1);
-		int size = ServletRequestUtils.getIntParameter(request, "size", 1);
-		boolean showContent = ServletRequestUtils.getBooleanParameter(request, "showContent", false);
-		// System.out.println(request.getRequestURL());
-		if (showContent) {
-			return ResponseEntity.ok(productService.getContent(page, size));
-		}
-		return ResponseEntity.ok(productService.info(request, page, size));
-	}
 
 }
