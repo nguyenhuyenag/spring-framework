fb00b796a128f193d0de72389dffcd8921d6b7e7
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-04-04 15:07:09 +0700

    #

--- spring-boot/spring-pagination/src/main/java/com/service/ProductService.java
+++ spring-boot/spring-pagination/src/main/java/com/service/ProductService.java
@@ -22,21 +22,39 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 
+	/*-
+	 * getSize() vs getNumberOfElements()  
+	 * 
+	 * For items = 55, page_size = 10
+	 * 
+	 * getSize() -> Return the Page size (current page size) if it is pageable
+	 * 
+	 * Example: A page can be defined to have 10 items. So, getSize() would return
+	 * 10 based on Page definition.
+	 * 
+	 * getNumberOfElements -> Returns the actual content size of a page
+	 * 
+	 * Example: The number of elements can be 10 or less than 10 based on the actual
+	 * data. The last page would return 5 items
+	 */
 	public Map<String, Object> info(HttpServletRequest request, int page, int size) {
 		Map<String, Object> map = new LinkedHashMap<>();
 		Page<Product> pagedResult = repository.findAll(PageRequest.of(page, size));
-		map.put("Total items", pagedResult.getTotalElements());
-		map.put("Total pages", pagedResult.getTotalPages());
-		map.put("Page number (current page)", pagedResult.getNumber());
-		map.put("Page size", pagedResult.getSize());
-		// map.put("isFirst", pagedResult.isFirst());
-		// map.put("isLast", pagedResult.isLast());
-		// map.put("hasContent", pagedResult.hasContent());
-		String url = request.getRequestURL() + "?" + request.getQueryString() + "&showContent=true";
-		map.put("Contents", url);
+		if (pagedResult.hasContent()) {
+			map.put("Total items", pagedResult.getTotalElements());
+			int totalPages = pagedResult.getTotalPages();
+			map.put("Total pages (0 -> " + (totalPages - 1) + ")", pagedResult.getTotalPages());
+			map.put("Page number (current page)", pagedResult.getNumber());
+			map.put("Capacity of page", pagedResult.getSize());
+			map.put("Current page size (<= capacity)", pagedResult.getNumberOfElements());
+			// map.put("isFirst", pagedResult.isFirst());
+			// map.put("isLast", pagedResult.isLast());
+			String url = request.getRequestURL() + "?" + request.getQueryString() + "&showContent=true";
+			map.put("Contents", url);
+		}
 		return map;
 	}
-	
+
 	public List<Product> getContent(int page, int size) {
 		Page<Product> pagedResult = repository.findAll(PageRequest.of(page, size));
 		return pagedResult.getContent();
--- spring-boot/spring-pagination/src/main/webapp/WEB-INF/views/menu.jsp
+++ spring-boot/spring-pagination/src/main/webapp/WEB-INF/views/menu.jsp
@@ -7,7 +7,7 @@
 		<div id="navbar" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
 				<li><a href="${CONTEXT_PATH}/product">Product</a></li>
-				<li><a href="${CONTEXT_PATH}/api/page-info?size=5&page=7">Page API</a></li>
+				<li><a href="${CONTEXT_PATH}/api/page-info?size=7&page=9">Page API</a></li>
 			</ul>
 		</div>
 	</div>
