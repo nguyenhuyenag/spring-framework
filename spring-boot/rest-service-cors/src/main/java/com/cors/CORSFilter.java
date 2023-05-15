51c22be48d913ec4981fdca7c1502c3fd59222d9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-05-15 14:53:02 +0700

    #

---
+++ spring-boot/rest-service-cors/src/main/java/com/cors/CORSFilter.java
@@ -0,0 +1,27 @@
+package com.cors;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CORSFilter extends GenericFilterBean implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+		httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
+		httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
+		httpServletResponse.setHeader("Access-Control-Allow-Credentials", "*");
+		httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
+}
---
+++ spring-boot/rest-service-cors/src/main/java/com/cors/RestServiceCorsApplication.java
@@ -0,0 +1,25 @@
+package com.cors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RestServiceCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceCorsApplication.class, args);
+	}
+
+    @Bean
+    FilterRegistrationBean<CORSFilter> filterRegistrationBean() {
+		FilterRegistrationBean<CORSFilter> filter = new FilterRegistrationBean<>();
+		filter.setFilter(new CORSFilter());
+		filter.setName("CORS Filter");
+		filter.addUrlPatterns("/*");
+		filter.setOrder(1);
+		return filter;
+	}
+
+}
