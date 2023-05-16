65db63973d06faa6b1e496eb1cbd5d9e72b2af38
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-05-16 10:32:15 +0700

    #

---
+++ spring-boot/spring-webflux/src/main/java/io/reflectoring/springwebflux/config/WebclientConfig.java
@@ -0,0 +1,47 @@
+package io.reflectoring.springwebflux.config;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
+
+@Configuration
+public class WebclientConfig {
+
+    @Bean
+    public WebClient getWebClientBuilder() throws SSLException {
+
+        // Define SSLContext in order to connect to secure REST API network
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+
+        // Create an HTTP Client. Overload with timeout config and LogLevel settings.
+        // Override the above SSLContext into the client
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 120 * 1000)
+                .doOnConnected(connection -> connection.addHandlerLast(new ReadTimeoutHandler(120 * 1000, TimeUnit.MILLISECONDS)))
+                .wiretap("reactor.netty.http.client.HttpClient", LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL)
+                .followRedirect(true)
+                .secure(sslContextSpec -> sslContextSpec.sslContext(sslContext));
+
+        // Add the above connector to the WebClient instance
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}
---
+++ spring-boot/spring-webflux/src/main/java/io/reflectoring/springwebflux/model/User.java
@@ -0,0 +1,27 @@
+package io.reflectoring.springwebflux.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode(of = {"id","name","department"})
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(value = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private double salary;
+    private String department;
+    
+}
