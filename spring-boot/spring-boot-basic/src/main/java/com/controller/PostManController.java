1bcdcc0a8a348f116976cceb581af3f7217bdad6
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-06-23 09:55:02 +0700

    #

--- spring-boot/spring-boot-basic/src/main/java/com/controller/PostManController.java
+++ spring-boot/spring-boot-basic/src/main/java/com/controller/PostManController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.util.Arrays;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,16 @@ public class PostManController {
 	/*-
 	 * Body form-data 
 	 * 
-	 * 		my_array 	1 
+	 * 		my_array 	1
 	 * 		my_array 	2
+	 * 
+	 * 		to_array	1, 2, 3
 	 * 
 	 */
 	@RequestMapping(value = "sending_array")
-	private ResponseEntity<?> test(String[] my_array) {
-		return ResponseEntity.ok(my_array);
+	private ResponseEntity<?> test(String[] my_array, Integer[] to_array) {
+		System.out.println(Arrays.toString(to_array));
+		return ResponseEntity.ok(to_array);
 	}
 
 }
