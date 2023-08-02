aa2863efe47a1aa652d64b0e38fb76cd682c2619
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-08-02 16:07:59 +0700

    #

---
+++ spring-boot/spring-boot-basic/src/main/java/com/tool/EntityUtils.java
@@ -0,0 +1,49 @@
+package com.tool;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Table;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Metamodel;
+
+import com.entity.User;
+
+public class EntityUtils {
+
+	/**
+	 * getTableName @Table(name = "t_user") -> 't_user'
+	 */
+	public static String getTableName(Class<?> entityClass) {
+		Table annotation = entityClass.getAnnotation(Table.class);
+		if (annotation != null) {
+			return annotation.name();
+		} else {
+			// Handle the case when the @Table annotation is not present
+			// or return the entity class name as the default table name.
+			return entityClass.getSimpleName();
+		}
+	}
+
+	/**
+	 * Returns the table name for a given entity type in the {@link EntityManager}.
+	 * 
+	 * @param em
+	 * @param entityClass
+	 * @return
+	 */
+	public static <T> String getTableName(EntityManager em, Class<T> entityClass) {
+		/*
+		 * Check if the specified class is present in the metamodel. Throws
+		 * IllegalArgumentException if not.
+		 */
+		Metamodel meta = em.getMetamodel();
+		EntityType<T> entityType = meta.entity(entityClass);
+		// Check whether @Table annotation is present on the class.
+		Table t = entityClass.getAnnotation(Table.class);
+		return t == null ? entityType.getName() : t.name();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("TableName: " + getTableName(User.class));
+	}
+
+}
