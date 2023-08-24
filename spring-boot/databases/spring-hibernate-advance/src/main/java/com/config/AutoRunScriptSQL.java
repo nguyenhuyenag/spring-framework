8123ba9790991ea2dc8accfb1367511636ee90c1
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-08-24 22:15:47 +0700

    #

--- spring-boot/databases/spring-hibernate-advance/src/main/java/com/config/AutoRunScriptSQL.java
+++ spring-boot/databases/spring-hibernate-advance/src/main/java/com/config/AutoRunScriptSQL.java
@@ -10,13 +10,22 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+/*-
+ * - Sử dụng `application.properties`
+ * 
+ * 		spring.datasource.initialization-mode=always
+ * 		# Tạo bảng
+ * 		spring.datasource.schema=classpath:jdbc/auto_run_schema.sql
+ * 		# Thêm dữ liệu
+ * 		spring.datasource.data=classpath:jdbc/auto_run_schema.sql
+ */
 @Configuration
 public class AutoRunScriptSQL {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(AutoRunScriptSQL.class);
 
 	@Bean
-	public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+	public DataSourceInitializer loadData(final DataSource dataSource) {
 		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
 		databasePopulator.addScript(new ClassPathResource("jdbc/auto_run_schema.sql"));
 		DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
--- spring-boot/databases/spring-hibernate-advance/src/main/resources/jdbc/auto_run_schema.sql
+++ spring-boot/databases/spring-hibernate-advance/src/main/resources/jdbc/auto_run_schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS t_user;
+-- DROP TABLE IF EXISTS t_user;
 
-CREATE TABLE t_user (
+CREATE TABLE IF NOT EXISTS t_user (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(5) DEFAULT NULL,
   `age` tinyint NOT NULL DEFAULT '0',
