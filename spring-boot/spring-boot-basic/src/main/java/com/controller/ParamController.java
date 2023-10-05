315bc3eafac823854c7d4288147f5abf3c8098e4
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-05 17:16:14 +0700

    #

--- spring-boot/spring-boot-basic/src/main/java/com/controller/ParamController.java
+++ spring-boot/spring-boot-basic/src/main/java/com/controller/ParamController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class ParamController {
 	@Autowired
 	private UserRepository repository;
 
-	@GetMapping("list-user")
+	@RequestMapping(value = "list-user", method = { RequestMethod.PUT, RequestMethod.POST })
 	private ResponseEntity<List<User>> getAll() {
 		List<User> list = repository.findAll();
 		// HttpHeaders responseHeaders = new HttpHeaders();
