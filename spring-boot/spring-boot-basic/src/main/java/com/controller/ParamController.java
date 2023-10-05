b40278c51372f625f6122c185740a80c3d37cec8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-05 09:20:12 +0700

    #

--- spring-boot/spring-boot-basic/src/main/java/com/controller/ParamController.java
+++ spring-boot/spring-boot-basic/src/main/java/com/controller/ParamController.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,75 +34,28 @@ public class ParamController {
 	}
 
 	/**
-	 * Annotation PathVariable được sử dụng để xử lý những URI động, có một hoặc
-	 * nhiều paramter bên trong URI
+	 * @PathVariable được sử dụng để xử lý những URI động, có một hoặc nhiều
+	 *               paramter bên trong URI
+	 * 
+	 *               /user/id/11 -> `11` được gọi là tham số đường dẫn
 	 */
-	@RequestMapping("/test1/{id}")
-	public String test1(@PathVariable("id") int id, Model model) {
-		model.addAttribute("id", id);
+
+	// One variable
+	@GetMapping("/test1/{id}")
+	public String test1(@PathVariable("id") int id) {
+		System.out.println("ID: " + id);
 		return "test1";
 	}
 
-	@RequestMapping("/test2/{id}/{name}")
-	public String test2(@PathVariable("id") int id, @PathVariable("name") String name, Model model) {
-		model.addAttribute("id", id);
-		model.addAttribute("name", name);
+	// Multiple variable
+	@GetMapping("/test2/{id}/{name}")
+	public String test2(@PathVariable("id") int id, @PathVariable("name") String name) {
+		System.out.println("ID: " + id);
+		System.out.println("Name: " + name);
 		return "test2";
 	}
 
-	/*-
-	 * /user/id/11	->	`11` được gọi là tham số đường dẫn
-	 */
-	@GetMapping("id/{id}")
-	// @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Was Not Found")
-	private ResponseEntity<User> getById(@PathVariable("id") int id) {
-		Optional<User> user = repository.findByIdNativeSQL(id);
-		if (user.isPresent()) {
-			return ResponseEntity.ok().body(user.get());
-		}
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
-
-	/*-
-	 * /user/find			(0)
-	 * /user/find?id=1 		(1)
-	 * 
-	 * @RequestParam(required = true) 		: (0) => 400 Bad Request: Required String parameter 'id' 
-	 * 										   is not present
-	 * 
-	 * @RequestParam(required = false)		: (0) => id = null
-	 * 										: Mặc định `required = true`
-	 * 
-	 * @RequestParam(defaultValue = "test") : (0) => id = test
-	 * 
-	 * String id
-	 * @RequestParam int id
-	 * @RequestParam("id") int id
-	 * @RequestParam(name = "id") String id
-	 * @RequestParam(value = "id") String id
-	 */
-	@GetMapping("find")
-	private ResponseEntity<String> getByIds(@RequestParam("id") String id) {
-		String result = "ID: " + id;
-		return new ResponseEntity<>(result, HttpStatus.OK);
-	}
-
-	/*-
-	 * /user/map-all?a=1&b=2&c=3
-	 */
-	@GetMapping("map-all")
-	private ResponseEntity<String> map(@RequestParam Map<String, String> params) {
-		Set<Entry<String, String>> entrys = params.entrySet();
-		for (Entry<String, String> e : entrys) {
-			System.out.println(e.getKey() + " => " + e.getValue());
-		}
-		String result = "Parameters are: " + entrys;
-		return ResponseEntity.status(HttpStatus.OK).body(result);
-	}
-
-	/*-
-	 * /api/employees/1/bar
-	 */
+	// Using Map for multiple variable: /api/employees/1/bar
 	@GetMapping("/api/employees/{id}/{name}")
 	@ResponseBody
 	public String getEmployeesByIdAndNameWithMapVariable(@PathVariable Map<String, String> pathVarsMap) {
@@ -113,28 +65,6 @@ public class ParamController {
 			return "ID: " + id + ", name: " + name;
 		}
 		return "Missing Parameters";
-	}
-
-	/*-
-	 * /user/set-all?name=ABC&name=DEF
-	 */
-	@GetMapping("set-all")
-	private ResponseEntity<String> set(@RequestParam Set<String> name) {
-		for (String s : name) {
-			System.out.println(s);
-		}
-		String result = "Parameters are: " + name;
-		return new ResponseEntity<>(result, HttpStatus.OK);
-	}
-
-	/*-
-	 * /user/list-all?id=1,2,3
-	 * /user/list-all?id=1&id=2
-	 */
-	@GetMapping("list-all")
-	private ResponseEntity<String> list(@RequestParam List<String> id) {
-		String result = "Parameters are: " + id;
-		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
 	/*-
@@ -150,16 +80,74 @@ public class ParamController {
 		return "ID: " + id;
 	}
 
-	/**
+	/*-
 	 * Setting @PathVariable as Not Required
 	 */
-	@GetMapping(value = { "/api/employeeswithoptional", "/api/employeeswithoptional/{id}" })
+	@GetMapping({ "/api/employeeswithoptional", "/api/employeeswithoptional/{id}" })
 	@ResponseBody
 	public String getEmployeesByIdWithOptional(@PathVariable Optional<String> id) {
-	    if (id.isPresent()) {
-	        return "ID: " + id.get();
-	    } else {
-	        return "ID missing";
-	    }
+		if (id.isPresent()) {
+			return "ID: " + id.get();
+		}
+		return "ID missing";
 	}
+
+	/**
+	 * @RequestParam
+	 */
+
+	/*-
+	 * For: /user/find
+	 * 		/user/find/id
+	 * 
+	 * 		@RequestParam(required = true) 			: 400 Bad Request if 'id' is not present
+	 * 		@RequestParam(required = false)			: id = null
+	 * 		@RequestParam(defaultValue = "test")	: id = test
+	 * 
+	 * - Các khai báo dưới đây là tương đương
+	 * 		
+	 * 		String id
+	 * 		@RequestParam int id
+	 * 		@RequestParam("id") int id
+	 * 		@RequestParam(name = "id") String id
+	 * 		@RequestParam(value = "id") String id
+	 */
+	@GetMapping("find")
+	private ResponseEntity<String> getByIds(@RequestParam("id") String id) {
+		String result = "ID: " + id;
+		return ResponseEntity.ok(result);
+	}
+
+	// /user/map-all?name=Zen&age=25&country=US
+	@GetMapping("map-all")
+	private ResponseEntity<String> map(@RequestParam Map<String, String> params) {
+		Set<Entry<String, String>> entrys = params.entrySet();
+		for (Entry<String, String> e : entrys) {
+			System.out.println(e.getKey() + " => " + e.getValue());
+		}
+		String result = "Parameters are: " + entrys;
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
+
+	/*-
+	 * /user/list-all?id=1,2,3
+	 * 
+	 * /user/list-all?id=1&id=2
+	 */
+	@GetMapping("list-all")
+	private ResponseEntity<String> list(@RequestParam List<String> id) {
+		String result = "Parameters are: " + id;
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	// /user/set-all?name=ABC&name=DEF
+	@GetMapping("set-all")
+	private ResponseEntity<String> set(@RequestParam Set<String> name) {
+		for (String s : name) {
+			System.out.println(s);
+		}
+		String result = "Parameters are: " + name;
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
 }
