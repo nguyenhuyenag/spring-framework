5c2e2624c1370db152279337fc945ca64a6d8406
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-06 21:20:59 +0700

    #

--- spring-boot/spring-boot-basic/src/main/java/com/boot/datasource/CreateDataSource.java
+++ spring-boot/spring-boot-basic/src/main/java/com/boot/datasource/CreateDataSource.java
@@ -2,24 +2,27 @@ package com.boot.datasource;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Configuration
+// @Configuration (tạm đóng, mở ra chạy lỗi do chưa cấu hình EntityManagerFactory)
 public class CreateDataSource {
 
-	private String url, username, password, driverClassName;
+	private String url, username, password;
 
-	@Bean
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+
+	@Bean(name = "data1Source")
 	public DataSource data1Source() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
 		dataSource.setDriverClassName(driverClassName);
 		return dataSource;
 	}
 
-	@Bean
+	@Bean(name = "data2Source")
 	public DataSource data2Source() {
 		return DataSourceBuilder.create() //
 				.url(url) //
