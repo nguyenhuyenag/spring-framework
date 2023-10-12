36dd71bb0447ef5f1c154a517eda15f37bf6691d
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-12 11:08:32 +0700

    #

--- spring-boot/http-request/src/main/java/com/util/URLQuery.java
+++ spring-boot/http-request/src/main/java/com/util/URLQuery.java
@@ -2,13 +2,15 @@ package com.util;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class URLQuery {
 
-	public static void whenUsingApacheUriBuilder() throws URISyntaxException {
+	public static void usingApacheUriBuilder() throws URISyntaxException {
 		String url = "https://www.youtube.com/watch";
 		URI uri = new URIBuilder(url) //
 				.addParameter("v", "3AtDnEC4zak")//
@@ -17,7 +19,7 @@ public class URLQuery {
 		System.out.println(uri.toString());
 	}
 
-	public static void whenUsingSpringUriComponentsBuilder() {
+	public static void usingSpringUriComponentsBuilder() {
 		String url = "https://www.youtube.com/watch";
 		URI uri = UriComponentsBuilder.fromUriString(url) //
 				.queryParam("v", "3AtDnEC4zak") //
@@ -27,9 +29,25 @@ public class URLQuery {
 		System.out.println(uri.toString());
 	}
 
-	public static void main(String[] args) throws URISyntaxException {
-		whenUsingApacheUriBuilder();
-		whenUsingSpringUriComponentsBuilder();
+	protected static URI encodeValue(String url) {
+		return UriComponentsBuilder.fromUriString(url).build().encode().toUri();
+	}
+
+	protected static String decode(String value) throws Exception {
+		return URLDecoder.decode(value, StandardCharsets.UTF_8.toString());
+	}
+
+	public static void encodeURL() throws Exception {
+		String url = "http://www.example.com/CEREC® Materials & Accessories/IPS Empress® CAD.pdf";
+		URI encodeValue = encodeValue(url);
+		System.out.println(encodeValue);
+		System.out.println(decode(encodeValue.toString()));
+	}
+
+	public static void main(String[] args) throws Exception {
+		usingApacheUriBuilder();
+		usingSpringUriComponentsBuilder();
+		encodeURL();
 	}
 
 }
