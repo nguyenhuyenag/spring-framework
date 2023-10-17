a4b182a2868694e737487fca80184f9d8de312e5
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-17 10:24:57 +0700

    #

--- spring-boot/spring-boot-rest/src/main/java/com/config/RateLimitConfiguration.java
+++ spring-boot/spring-boot-rest/src/main/java/com/config/RateLimitConfiguration.java
@@ -1,28 +1,8 @@
 package com.config;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.util.concurrent.RateLimiter;
-
-/*-
-	@Bean 
-	public Map<String, RateLimiter> rateLimiters() { 
-		Map<String, RateLimiter> rateLimiters = new ConcurrentHashMap<>();
-		rateLimiters.put("/api/slow-operation", new RateLimiter(5.0));
-		rateLimiters.put("/api/fast-operation", new RateLimiter(20.0));
-		return rateLimiters;
-	}
-	
-	@Autowired
-	private Map<String, RateLimiter> rateLimiters;
-	
-	RateLimiter rateLimiter = rateLimiters.get("/api/slow-operation");
-	
- */
 @Configuration
 public class RateLimitConfiguration {
 
@@ -30,24 +10,5 @@ public class RateLimitConfiguration {
 	public com.google.common.util.concurrent.RateLimiter rateLimiterByGuava() {
 		return com.google.common.util.concurrent.RateLimiter.create(1); // 1 requests per second
 	}
-	
-	@Bean 
-	public Map<String, RateLimiter> rateLimiters() { 
-		Map<String, RateLimiter> rateLimiters = new ConcurrentHashMap<>();
-		rateLimiters.put("LIMIT_1", RateLimiter.create(1));
-		rateLimiters.put("LIMIT_2", RateLimiter.create(2));
-		return rateLimiters;
-	}
-
-//	@Bean
-//	public io.github.resilience4j.ratelimiter.RateLimiter rateLimiter() {
-//		RateLimiterConfig config = RateLimiterConfig.custom() //
-//				.limitForPeriod(1) //
-//				.limitRefreshPeriod(Duration.ofSeconds(1)) //
-//				.timeoutDuration(Duration.ofMillis(0)) //
-//				.build();
-//
-//		return io.github.resilience4j.ratelimiter.RateLimiter.of("myRateLimiter", config);
-//	}
 
 }
--- spring-boot/spring-boot-rest/src/main/java/com/controller/LimitRequests.java
+++ spring-boot/spring-boot-rest/src/main/java/com/controller/LimitRequests.java
@@ -13,21 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class LimitRequests {
 
 	@Autowired
-	private com.google.common.util.concurrent.RateLimiter rateLimiter;
+	protected com.google.common.util.concurrent.RateLimiter rateLimiter;
 
 	@GetMapping("limit-requests")
 	public ResponseEntity<?> limitRequests() {
+		// RateLimiter rateLimiter = RateLimiter.create(1.0); // Not working
 		if (rateLimiter.tryAcquire()) {
 			// Logic here
 			return ResponseEntity.ok("Success!");
 		}
 		return ResponseEntity.ok(HttpStatus.TOO_MANY_REQUESTS);
 	}
-	
-//	@io.github.resilience4j.ratelimiter.annotation.RateLimiter(name = "myRateLimiter")
-//    @GetMapping("/limited-endpoint")
-//    public String limitedEndpoint() {
-//        return "This endpoint is rate-limited to 10 requests per second.";
-//    }
 
 }
