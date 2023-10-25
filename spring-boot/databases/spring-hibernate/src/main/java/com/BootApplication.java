7fb8bb8aebac77eb31d198ada535cfe6a0666210
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-10-25 21:20:57 +0700

    #

--- spring-boot/databases/spring-hibernate/src/main/java/com/BootApplication.java
+++ spring-boot/databases/spring-hibernate/src/main/java/com/BootApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // @EnableJpaAuditing
 @SpringBootApplication
-public class Application {
+public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(BootApplication.class, args);
 	}
 
 }
