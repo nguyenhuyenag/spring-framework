47bea2764b0140e2f233d2183594ed26b50380b8
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-05 13:17:49 +0700

    #

---
+++ spring-boot/spring-rss/src/main/java/com/reader/FeedConsumer.java
@@ -0,0 +1,31 @@
+package com.reader;
+
+import java.net.URL;
+import java.util.List;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+public class FeedConsumer {
+
+	public static void main(String[] args) throws Exception {
+		String url = "https://stackjava.com/feed";
+		// String url = "https://vnexpress.net/rss/tin-moi-nhat.rss";
+		// String url = "https://www.24h.com.vn/upload/rss/trangchu24h.rss";
+		URL feedUrl = new URL(url);
+		SyndFeedInput input = new SyndFeedInput();
+		try (XmlReader xmlReader = new XmlReader(feedUrl);) {
+			SyndFeed feed = input.build(xmlReader);
+			for (SyndEntry entry : (List<SyndEntry>) feed.getEntries()) {
+				System.out.println("-----------------------------");
+				System.out.println("Title : " + entry.getTitle());
+				System.out.println("Author: " + entry.getAuthor());
+				// System.out.println("Description : " + entry.getDescription());
+				System.out.println("Link  : " + entry.getLink());
+			}
+		}
+	}
+
+}
