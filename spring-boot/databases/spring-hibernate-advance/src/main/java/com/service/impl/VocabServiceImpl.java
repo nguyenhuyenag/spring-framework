ee5f1d672c169d0305d3fd6bcc24b217a19a9c47
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2023-12-07 16:58:51 +0700

    #

--- spring-boot/databases/spring-hibernate-advance/src/main/java/com/service/impl/VocabServiceImpl.java
+++ spring-boot/databases/spring-hibernate-advance/src/main/java/com/service/impl/VocabServiceImpl.java
@@ -9,10 +9,14 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +33,15 @@ public class VocabServiceImpl implements CriteriaService {
 	@Autowired
 	private EntityManager entityManager;
 
+	Session session = entityManager.unwrap(Session.class);
+	@SuppressWarnings("deprecation")
+	Criteria criteria = session.createCriteria(Vocabulary.class);
+	
+	// Criteria
+	CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+	CriteriaQuery<Vocabulary> criteriaQuery = criteriaBuilder.createQuery(Vocabulary.class);
+	Root<Vocabulary> root = criteriaQuery.from(Vocabulary.class);
+
 	@Override
 	public void CollectionParam() {
 		List<String> words = Arrays.asList("bake", "concept", "theater");
@@ -39,17 +52,60 @@ public class VocabServiceImpl implements CriteriaService {
 	@Override
 	public void findUserByEmails() {
 		Set<String> emails = new HashSet<>(Arrays.asList("ac", "an", "ar"));
-		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Vocabulary> query = cb.createQuery(Vocabulary.class);
-		Root<Vocabulary> user = query.from(Vocabulary.class);
+		// CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		// CriteriaQuery<Vocabulary> query = cb.createQuery(Vocabulary.class);
+		Root<Vocabulary> user = criteriaQuery.from(Vocabulary.class);
 		Path<String> emailPath = user.get("word");
 		List<Predicate> predicates = new ArrayList<>();
 		for (String email : emails) {
-			predicates.add(cb.like(emailPath, email));
+			predicates.add(criteriaBuilder.like(emailPath, email));
 		}
-		query.select(user).where(cb.or(predicates.toArray(new Predicate[predicates.size()])));
-		List<Vocabulary> list = entityManager.createQuery(query).getResultList();
+		criteriaQuery.select(user) //
+				.where(criteriaBuilder.or(predicates.toArray(new Predicate[predicates.size()])));
+
+		List<Vocabulary> list = entityManager.createQuery(criteriaQuery).getResultList();
 		list.forEach(t -> System.out.println(t));
+	}
+
+//	public void like() {
+//		Session session = sessionFactory.openSession();
+//		Criteria criteria = session.createCriteria(Vocabulary.class);
+//		criteria.add(Restrictions.like("firstName", "John"));
+//		List<Vocabulary> employees = criteria.list();
+//		session.close();
+//	}
+	
+	public void operatorByRestrictions() {
+		// Add EQUAL criteria (eq, lt, le, gt, ge)
+		criteria.add(Restrictions.eq("address", "usa"));
+		
+		// LIKE 
+		criteria.add(Restrictions.like("name", "%th%"));
+		
+		// BETWEEN
+		criteria.add(Restrictions.between("dateOfBirth", "startDate", "endDate"));
+		
+		// NULL, NOT NULL,...
+		criteria.add(Restrictions.isNotNull("description"));
+	}
+
+	public void operatorsByExpression() {
+		// Add EQUAL criteria
+		Predicate equalPredicate = criteriaBuilder.equal(root.get("fieldName"), "valueToMatch");
+		criteriaQuery.where(equalPredicate);
+
+		// Add LIKE criteria
+		String partialString = "";
+		Predicate likePredicate = criteriaBuilder.like(root.get("fieldName"), "%" + partialString + "%");
+		criteriaQuery.where(likePredicate);
+
+		// Add sorting
+		Order order = criteriaBuilder.asc(root.get("fieldName")); // Change to desc for descending order
+		criteriaQuery.orderBy(order);
+
+		// Execute the query
+		List<Vocabulary> resultList = entityManager.createQuery(criteriaQuery).getResultList();
+		System.out.println(resultList.size());
 	}
 
 }
