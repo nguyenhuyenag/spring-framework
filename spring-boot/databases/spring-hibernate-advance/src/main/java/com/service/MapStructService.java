4b04ccb8b87641e1bdb4b81863cadca048460417
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-04-23 13:51:33 +0700

    #

--- spring-boot/databases/spring-hibernate-advance/src/main/java/com/service/MapStructService.java
+++ spring-boot/databases/spring-hibernate-advance/src/main/java/com/service/MapStructService.java
@@ -10,10 +10,11 @@ import com.entity.Category;
  * How to run? 	-> mvn clean install 
  * 				-> View /target/generated-sources/annotations/
  */
-@Mapper
+@Mapper(componentModel = "spring") // Đánh dấu một mapping interface và cho phép MapStrust hoạt động trên interface này
 public interface MapStructService {
 
-	@Mapping(source = "statusCategory", target = "status")
+	// @Mapping(target = "status", ignore = true)
+	@Mapping(source = "statusCategory", target = "status") // Map 2 field khác nhau
 	@Mapping(source = "createDate", target = "createDate", dateFormat = "dd/MM/yyyy")
 	@Mapping(source = "updateTime", target = "updateTime", dateFormat = "dd-MM-yyyy HH:mm:ss")
 	CategoryResDto categoryToCategoryDto(Category category);
