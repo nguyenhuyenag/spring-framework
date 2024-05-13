582a0b75fa3d9418a6e370a0286af07fc9dd97ee
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-05-13 16:57:29 +0700

    #

--- spring-boot/multi-threading/src/main/java/com/core/create/UsingRunnable.java
+++ spring-boot/multi-threading/src/main/java/com/core/create/UsingRunnable.java
@@ -2,32 +2,32 @@ package com.core.create;
 
 public class UsingRunnable implements Runnable {
 
-	@Override
-	public void run() {
-		System.out.println("ThreadName -> " + Thread.currentThread().getName());
-		System.out.println("Thread running");
-	}
+    @Override
+    public void run() {
+        System.out.println("ThreadName -> " + Thread.currentThread().getName());
+        System.out.println("Thread running");
+    }
 
-	/**
-	 * Không tạo ra thread mới, phương thức run() vẫn thực thi trên chính thread
-	 * hiện tại
-	 */
-	public static void incorrect() {
-		System.out.println("ThreadName -> " + Thread.currentThread().getName());
-		Runnable task = new UsingRunnable();
-		task.run();
-	}
+    /**
+     * Không tạo ra thread mới, phương thức run() vẫn thực thi trên chính thread
+     * hiện tại
+     */
+    public static void incorrect() {
+        System.out.println("ThreadName -> " + Thread.currentThread().getName());
+        Runnable task = new UsingRunnable();
+        task.run();
+    }
 
-	public static void correct() {
-		System.out.println("ThreadName -> " + Thread.currentThread().getName());
-		Runnable task = new UsingRunnable();
-		Thread t = new Thread(task);
-		t.start();
-	}
+    public static void correct() {
+        System.out.println("ThreadName -> " + Thread.currentThread().getName());
+        Runnable task = new UsingRunnable();
+        Thread t = new Thread(task);
+        t.start();
+    }
 
-	public static void main(String args[]) {
-		correct();
-		// incorrect();
-	}
+    public static void main(String[] args) {
+        // correct();
+        incorrect();
+    }
 
 }
--- spring-boot/multi-threading/src/main/java/com/core/create/UsingThread.java
+++ spring-boot/multi-threading/src/main/java/com/core/create/UsingThread.java
@@ -2,51 +2,49 @@ package com.core.create;
 
 /**
  * Có 2 cách tạo thread là `extends Thread` hoặc `implements Runnable`
+ * <p>
+ * Cách implements Runnable được ưa chuộng hơn vì chỉ có thể extends được 1 class
  */
 public class UsingThread extends Thread {
 
-	@Override
-	public void run() {
-		System.out.println("ThreadName -> " + Thread.currentThread().getName());
-		try {
-			for (int i = 5; i > 0; i--) {
-				System.out.println(i);
-				Thread.sleep(100); // thread sleep
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void run() {
+        System.out.println("ThreadName -> " + Thread.currentThread().getName());
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println(i);
+                Thread.sleep(100); // thread sleep
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void cach2() {
-		Thread task = new Thread() {
-			@Override
-			public void run() {
-				System.out.println("ThreadName -> " + Thread.currentThread().getName());
-				try {
-					for (int i = 5; i > 0; i--) {
-						System.out.println(i);
-						Thread.sleep(100); // thread sleep
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		Thread t = new Thread(task);
-		t.start();
-	}
+    public static void cach1() {
+        UsingThread myThread = new UsingThread();
+        myThread.start(); // Dùng start() mới chính xác
+        // myThread.run(); // Không tạo ra luồng mới, chương trình vẫn chạy trên luồng hiện tại
+    }
 
-	public static void cach1() {
-		UsingThread myThread = new UsingThread();
-		Thread t = new Thread(myThread);
-		t.start();
-	}
+    public static void cach2() {
+        Thread task = new Thread(() -> {
+            System.out.println("ThreadName -> " + Thread.currentThread().getName());
+            try {
+                for (int i = 5; i > 0; i--) {
+                    System.out.println(i);
+                    Thread.sleep(100); // thread sleep
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        task.start();
+    }
 
-	public static void main(String args[]) {
-		System.out.println("ThreadName -> " + Thread.currentThread().getName());
-		cach1();
-		cach2();
-	}
+    public static void main(String[] args) {
+        System.out.println("ThreadName -> " + Thread.currentThread().getName());
+        cach1();
+        // cach2();
+    }
 
 }
