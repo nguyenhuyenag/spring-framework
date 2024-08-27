d0754f30bffadc11d4ff09722883d5ca4d1050a9
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-08-27 11:17:19 +0700

    #

---
+++ spring-boot/spring-boot-basic/src/main/java/com/boot/component/autowired/AutowiredStatic.java
@@ -0,0 +1,23 @@
+package com.boot.component.autowired;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutowiredStatic {
+	
+	
+	private static ResourceBundleMessageSource messageSource;
+
+    private AutowiredStatic(@Autowired ResourceBundleMessageSource messageSource) {
+    	AutowiredStatic.messageSource = messageSource;
+    }
+
+    public static void getMessage(String code) {
+        System.out.println(messageSource.toString());
+    }
+	
+}
