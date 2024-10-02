54b8dabf59a8edf20be974cc59875080e1950527
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-10-02 09:36:45 +0700

    #

--- spring-boot/spring-ftp/src/main/java/com/controller/AjaxController.java
+++ spring-boot/spring-ftp/src/main/java/com/controller/AjaxController.java
@@ -1,17 +1,26 @@
 package com.controller;
 
+import com.entity.FileStore;
 import com.service.FileStoreService;
+import com.util.Base64Utils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/ftp")
+@RequestMapping("ftp")
 public class AjaxController {
 
     private final FileStoreService fileStoreService;
@@ -46,41 +55,41 @@ public class AjaxController {
 //        return "upload-ajax";
 //    }
 
-//    // Download using Ajax
-//    @GetMapping(value = "/download-ajax")
-//    public String downloadAjax(Model model) {
-//        List<FileStore> files = fileStoreService.findAll();
-//        model.addAttribute("files", files);
-//        return "download-ajax";
-//    }
+    // Download using Ajax
+    @GetMapping(value = "/download-ajax")
+    public String downloadAjax(Model model) {
+        List<FileStore> files = fileStoreService.findAll();
+        model.addAttribute("files", files);
+        return "download-ajax";
+    }
 
-//    @ResponseBody
-//    @PostMapping(value = "/download-ajax")
-//    public void downloadAjax(HttpServletResponse response, String fileId) throws Exception {
-//        try {
-//            FileStore fileInfo = fileStoreService.findByFileId(fileId);
-//            String fileBase64 = fileInfo.getFileBase64();
-//
-//            byte[] fileData = Base64Utils.decodeToByte(fileBase64);
-//            File tempFile = File.createTempFile("tmp_", fileInfo.getFileName());
-//            Files.write(tempFile.toPath(), fileData);
-//            // Có thể dùng cách tương tự ở trên. Ở đây dùng TempFile để test
-//            // guessContentTypeFromStream()
-//            try (FileInputStream in = new FileInputStream(tempFile)) {
-//                // Set file to header
-//                response.setContentType(URLConnection.guessContentTypeFromStream(in));
-//                response.setContentLength(fileData.length);
-//                response.setHeader("Content-Disposition", "attachment; filename=\"" + fileInfo.getFileName() + "\"");
-//                FileCopyUtils.copy(in, response.getOutputStream());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } finally {
-//                tempFile.deleteOnExit();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @ResponseBody
+    @PostMapping(value = "/download-ajax")
+    public void downloadAjax(HttpServletResponse response, String fileId) throws Exception {
+        try {
+            FileStore fileInfo = fileStoreService.findByFileId(fileId);
+            String fileBase64 = fileInfo.getFileBase64();
+
+            byte[] fileData = Base64Utils.decodeToByte(fileBase64);
+            File tempFile = File.createTempFile("tmp_", fileInfo.getFileName());
+            Files.write(tempFile.toPath(), fileData);
+            // Có thể dùng cách tương tự ở trên. Ở đây dùng TempFile để test
+            // guessContentTypeFromStream()
+            try (FileInputStream in = new FileInputStream(tempFile)) {
+                // Set file to header
+                response.setContentType(URLConnection.guessContentTypeFromStream(in));
+                response.setContentLength(fileData.length);
+                response.setHeader("Content-Disposition", "attachment; filename=\"" + fileInfo.getFileName() + "\"");
+                FileCopyUtils.copy(in, response.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                tempFile.deleteOnExit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 //    @PostMapping(value = "/download-ajax-base64")
 //    public ResponseEntity<?> downloadAjaxBase64(String fileId) throws Exception {
--- spring-boot/spring-ftp/src/main/java/com/controller/DownloadController.java
+++ spring-boot/spring-ftp/src/main/java/com/controller/DownloadController.java
@@ -11,9 +11,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("ftp")
