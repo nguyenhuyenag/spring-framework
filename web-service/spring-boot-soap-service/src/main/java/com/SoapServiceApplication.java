cf10d502cdbfc236e717f8c3d3ba865daf449ce3
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-10-10 13:26:14 +0700

    #

--- web-service/spring-boot-soap-service/src/main/java/com/SoapServiceApplication.java
+++ web-service/spring-boot-soap-service/src/main/java/com/SoapServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootApplication {
+public class SoapServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		SpringApplication.run(SoapServiceApplication.class, args);
 	}
 	
 }
