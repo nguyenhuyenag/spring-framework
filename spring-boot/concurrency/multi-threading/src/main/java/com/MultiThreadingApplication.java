60f3d418cca191c92f350f6c14510f3c34c40d64
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-12-09 21:13:42 +0700

    #

--- spring-boot/concurrency/multi-threading/src/main/java/com/MultiThreadingApplication.java
+++ spring-boot/concurrency/multi-threading/src/main/java/com/MultiThreadingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MultiThreadingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MultiThreadingApplication.class, args); // .close();
+		SpringApplication.run(MultiThreadingApplication.class, args);
 	}
 
 }
