f54350736a236fa559833046fb23d41c8af89c35
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2024-12-11 09:55:11 +0700

    #

--- spring-boot/notifications/mail-service/src/main/java/com/mail/config/AsynchronousConfig.java
+++ spring-boot/notifications/mail-service/src/main/java/com/mail/config/AsynchronousConfig.java
@@ -9,14 +9,18 @@ public class AsynchronousConfig {
 
     @Bean(name = "mailSenderTaskExecutor")
     public ThreadPoolTaskExecutor mailTaskExecutor() {
+        // @formatter:off
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);    // Số thread chính
         executor.setMaxPoolSize(10);    // Số thread tối đa
         executor.setQueueCapacity(50);  // Dung lượng hàng đợi
+
         executor.setWaitForTasksToCompleteOnShutdown(true); // Đợi xử lý hết tác vụ trước khi shutdown
         executor.setAwaitTerminationSeconds(30);            // Thời gian chờ tối đa khi shutdown
+
         executor.setThreadNamePrefix("MailSenderTaskExecutor-");
         executor.initialize();
+        // @formatter:on
         return executor;
     }
 
