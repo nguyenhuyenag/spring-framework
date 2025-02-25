f7f0a02d0bfc180c11dc8a533da32a8b94c9c893
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2025-02-25 22:17:04 +0700

    #

--- spring-boot/spring-boot-basic/src/main/java/com/boot/runonce/RunOnceApplicationReadyEvent.java
+++ spring-boot/spring-boot-basic/src/main/java/com/boot/runonce/RunOnceApplicationReadyEvent.java
@@ -5,7 +5,10 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /*
-    Phương thưc chỉ chạy 1 lần
+    - Phương thưc chỉ chạy 1 lần
+
+    - ApplicationReadyEvent sẽ chạy cuối cùng, thích hợp cho
+    việc sử dụng các biến ConfigReader.CHILKAT_LICENSE_KEY
  */
 @Component
 public class RunOnceApplicationReadyEvent {
